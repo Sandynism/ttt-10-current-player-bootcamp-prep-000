7f8ed6c26c78bdc6266e7436207804b86a4a97bd
Sandy Yeung <sandynism@gmail.com>
2017-12-14 00:40:57 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 def turn_count(board)
   counter = 0
-  board.each {|xo| player == "X" || player == "O" ? counter +=1 : nil}
+  board.each {|player| player == "X" || player == "O" ? counter +=1 : nil}
   counter
 end
