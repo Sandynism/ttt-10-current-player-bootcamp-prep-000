4e146e6973f9e07a1ad9cf47d2709033c5956e33
Sandy Yeung <sandynism@gmail.com>
2017-12-14 00:45:11 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,3 +3,7 @@ def turn_count(board)
   board.each {|player| player == "X" || player == "O" ? counter +=1 : nil}
   counter
 end
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end
